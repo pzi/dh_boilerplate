ff0cd5c4fa123351a73c768122fb13fdc83477b5
David Hellmann <davidhellmann.com@gmail.com>
2014-11-13 02:34:26 +0100

    Done

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 dh_boilerplate
 ==============
 
+## Sass
 - Bourbon
+- Custom Grid
+- Custom Mixins
+- Custom Functions
+
+## JS
 - Modernizr
 - jQuery 2.1.1
---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Remove NavBar from iOS
+if( !window.location.hash && window.addEventListener ){
+  window.addEventListener( "load",function() {
+    setTimeout(function(){
+      window.scrollTo(0, 0);
+    }, 0);
+  });
+  window.addEventListener( "orientationchange",function() {
+    setTimeout(function(){
+      window.scrollTo(0, 0);
+    }, 0);
+  });
+}
+
+$(document).ready(function(){
+
+
+
+});
